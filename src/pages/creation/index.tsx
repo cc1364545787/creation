c9ef8e403a7b0f92ecca9568ff3473cfae93dae8
lybics <1364545787@qq.com>
2024-04-12 17:35:42 +0800

    feat: 更新依赖等

--- src/pages/creation/index.tsx
+++ src/pages/creation/index.tsx
@@ -1,9 +1,13 @@
 import styles from './index.less';
+import { Button } from 'antd';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className={styles.title}>create index</h1>
+    <div className={styles.main}>
+      {/* <h1 className={styles.title}>create index</h1> */}
+    <div className={styles.title}>
+    <Button type="primary">进入创作平台</Button>
+    </div>
     </div>
   );
 }
