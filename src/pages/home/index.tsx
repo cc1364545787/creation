6e0498cb23c9bcb0deeb939e11f6a9a0677a4bf1
lybics <1364545787@qq.com>
2024-05-07 22:17:42 +0800

    feat: 首页开始创作部分内容开发

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,8 @@ import styles from './index.less';
 import IndexPage from '@/components/index';
 import bannerFigure from '@/assets/home/img_white_collar.png';
 import imgLogo from '@/assets/home/img_logo.png';
+import startCreate from '@/assets/home/font_start_creating_sel.png';
+import startCreateIcon from '@/assets/home/icon_start_creating.png';
 
 const HomePage = () => {
   return (
@@ -13,7 +15,20 @@ const HomePage = () => {
           <img className={styles.personImg} src={bannerFigure} />
         </div>
         <div>
-          <div>热门分类</div>
+          <div className={styles.nav}>
+            <div className={styles.classBtn}>热门分类</div>
+
+            <div className={styles.startCreate}>
+              <div className={styles.startCreateLeft}>
+                <img className={styles.leftText1} src={startCreate} />
+                <span className={styles.leftText2}>高效短视频创作</span>
+              </div>
+              <div className={styles.startCreateRight}>
+                <img className={styles.rightIcon} src={startCreateIcon} />
+              </div>
+            </div>
+
+          </div>
           <div>
           <img className={styles.personImg} src={bannerFigure} />
           </div>
