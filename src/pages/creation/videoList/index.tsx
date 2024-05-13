aaffe0a22ff2a356a508f7a197bc4a332f5f37d8
lybics <1364545787@qq.com>
2024-05-13 20:43:13 +0800

    feat: 添加创作中心的视频封面

--- src/pages/creation/videoList/index.tsx
+++ src/pages/creation/videoList/index.tsx
@@ -1,9 +1,29 @@
-// import styles from './index.less';
+import styles from './index.less';
+import sss from '../../../assets/create_center/video_image.jpeg'
 
 const CreationVideoPage = () => {
-  return (
-      <div>
-        videolist
+    const config = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    return (
+      <div className={styles.main}>
+        {Array.isArray(config) ? (
+          config.map((_) => (
+            <div className={styles.videoBox}>
+                <div className={styles.videoContent}>
+                  <img className={styles.videoContentImg} src={sss} />
+                </div>
+                <div className={styles.videoMsg}>
+                  <div className={styles.videoMsgTitle}>
+                    视频名称
+                  </div>
+                  <div className={styles.videoMsgTime}>
+                    2024/12/2 12:11:23
+                  </div>
+                </div>
+            </div>
+              ))
+            ) : (
+              <></>
+        )}
       </div>
   );
 }
